9ccb46b40b7df7a3149f9857913192af3a429391
l.maslovs <leonids@printify.com>
2018-09-18 03:11:45 +0300

    add dt2018 and slackin2018 dns fqdn

--- 30_dns.tf
+++ 30_dns.tf
@@ -1,3 +1,13 @@
+resource "cloudflare_zone_settings_override" "devternity-com-settings" {
+  name   = "devternity.com"
+
+  settings {
+    tls_1_3 = "on"
+    automatic_https_rewrites = "on"
+    always_use_https="off"
+    ssl = "flexible"
+  }
+}
 
 resource "cloudflare_record" "sales" {
   domain = "devternity.com"
@@ -13,4 +23,22 @@ resource "cloudflare_record" "dashboard" {
   value  = "${aws_lightsail_instance.devternity_public_dashboard.public_ip_address}"
   type   = "A"
   ttl    = 120
+}
+
+resource "cloudflare_record" "dt2018-cname" {
+  domain = "devternity.com"
+  name   = "dt2018"
+  value  = "gh-travis-master.fqhrnw2tbq.eu-west-1.elasticbeanstalk.com"
+  type   = "CNAME"
+  ttl    = 120
+  proxied = true
+}
+
+resource "cloudflare_record" "slackin2018-cname" {
+  domain = "devternity.com"
+  name   = "slackin2018"
+  value  = "gh-dt2018-slackin.fqhrnw2tbq.eu-west-1.elasticbeanstalk.com"
+  type   = "CNAME"
+  ttl    = 120
+  proxied = true
 }
